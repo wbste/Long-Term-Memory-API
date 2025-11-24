7d5a6c6a047e7973ffbab8d986f11627d09a588c
jakops88-hub <jakops88@gmail.com>
2025-11-24 11:59:45 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,10 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
-  testMatch: ['**/?(*.)+(spec|test).ts'],
+  testRegex: '(/tests/.*|(\\\\.|/)(test|spec))\\\\.ts$',
   moduleFileExtensions: ['ts', 'js', 'json'],
   resetMocks: true,
   clearMocks: true,
   coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
-  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/', '<rootDir>/tests/.*\\.js$']
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts']
 };
